bb86e89d9e893b5f6135d674977d6b66066b819c
shahelaojieraozhi <1445692095@qq.com>
2022-05-22 21:17:23 +0800

    第一次提交

--- 遍历删除文件/os.walk()用法.py
+++ 遍历删除文件/os.walk()用法.py
@@ -1,14 +1,36 @@
 import os
 
-# dirname = './train2017'
-# for maindir, subdir, file_name_list in os.walk(dirname):
-#     for filename in file_name_list:
-#         apath = os.path.join(maindir, filename)
-#         print(apath)
+# # 使用os.walk扫描目录
+# for curDir, dirs, files in os.walk("./os.walk_test"):
+#     print("====================")
+#     print("现在的目录：" + curDir)
+#     print("该目录下包含的子目录：" + str(dirs))
+#     print("该目录下包含的文件：" + str(files))
 
-# 使用os.walk扫描目录
+# # 使用os.walk自底向上扫描目录
+# import os
+#
+# for curDir, dirs, files in os.walk("./os.walk_test", topdown=False):
+#     print("====================")
+#     print("现在的目录：" + curDir)
+#     print("该目录下包含的子目录：" + str(dirs))
+#     print("该目录下包含的文件：" + str(files))
+
+# 我们还可以利用os.walk输出test文件夹下所有的文件：
+
+# # 使用os.walk输出某个目录下的所有文件
+# import os
+#
+# for curDir, dirs, files in os.walk("./os.walk_test"):
+#     for file in files:
+#         print(os.path.join(curDir, file))
+
+# 同样地，我们也可以利用os.walk输出test文件夹下所有的子目录（子文件夹）：
+
+# 使用os.walk输出所有的目录
+import os
+
 for curDir, dirs, files in os.walk("./os.walk_test"):
-    print("====================")
-    print("现在的目录：" + curDir)
-    print("该目录下包含的子目录：" + str(dirs))
-    print("该目录下包含的文件：" + str(files))
+    for dir in dirs:
+        print(dir)
+
