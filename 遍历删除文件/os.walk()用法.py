fdb442f6f25081cd97ca167c3cd1e0a5a675563e
shahelaojieraozhi <1445692095@qq.com>
2022-05-22 20:32:30 +0800

    第一次提交

--- 遍历删除文件/os.walk()用法.py
+++ 遍历删除文件/os.walk()用法.py
@@ -1,11 +1,14 @@
 import os
 
-dirname = './train2017'
-for maindir, subdir, file_name_list in os.walk(dirname):
-    for filename in file_name_list:
-        apath = os.path.join(maindir, filename)
-    for subdir in subdir:
+# dirname = './train2017'
+# for maindir, subdir, file_name_list in os.walk(dirname):
+#     for filename in file_name_list:
+#         apath = os.path.join(maindir, filename)
+#         print(apath)
 
-        apath1 = os.path.join(subdir, filename)
-
-
+# 使用os.walk扫描目录
+for curDir, dirs, files in os.walk("./os.walk_test"):
+    print("====================")
+    print("现在的目录：" + curDir)
+    print("该目录下包含的子目录：" + str(dirs))
+    print("该目录下包含的文件：" + str(files))
